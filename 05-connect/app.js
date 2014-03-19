ccaa3f27f283438e3d0af69fedadb048e9ef0d14
Golo Roden <webmaster@goloroden.de>
2014-03-19 14:47:55 +0100

    Fixed some typos.

--- 05-connect/app.js
+++ 05-connect/app.js
@@ -4,11 +4,7 @@ var http = require('http');
 
 var connect = require('connect');
 
-var logger = require('./logger');
-
 var app = connect();
-
-app.use(logger);
 
 app.use(function (req, res, next) {
   console.log(req.method + ': ' + req.url);
