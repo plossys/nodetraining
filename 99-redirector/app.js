214860e7c5d8bddbbedd6bd7c1c230e4c6295984
Golo Roden <webmaster@goloroden.de>
2014-03-19 12:01:46 +0100

    Initial commit of redirection service.

---
+++ 99-redirector/app.js
@@ -0,0 +1,19 @@
+var http = require('http');
+
+var redirects = {
+  g: 'http://www.google.de',
+  h: 'http://www.heise.de/newsticker'
+};
+
+var server = http.createServer(function (req, res) {
+  var alias = req.url.substring(1);
+
+  res.writeHead(302, {
+    location: redirects[alias]
+  });
+  res.end();
+});
+
+server.listen(3000, function () {
+  console.log('Node.js server listening on port 3000');
+});
