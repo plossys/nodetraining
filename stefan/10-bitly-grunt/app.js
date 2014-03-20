5d261bba1559a9893d1b0e31b69cb244d722dd9c
Stefan Scherer <stefan@Stefans-MacBook-Pro.local>
2014-03-21 00:12:16 +0100

    vim ,e esformatter

--- stefan/10-bitly-grunt/app.js
+++ stefan/10-bitly-grunt/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var http = require('http'),
-    path = require('path');
+  path = require('path');
 
 var express = require('express');
 
@@ -14,20 +14,22 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger({ level: 'INFO' }));
+app.use(logger({
+  level: 'INFO'
+}));
 
-app.get('/', function (req, res) {
-  redirects.getAll(function (err, result) {
+app.get('/', function(req, res) {
+  redirects.getAll(function(err, result) {
     res.render('index', {
       redirects: result
     });
   });
 });
 
-app.get('/:alias', function (req, res) {
+app.get('/:alias', function(req, res) {
   var alias = req.params.alias;
 
-  redirects.getBy(alias, function (err, url) {
+  redirects.getBy(alias, function(err, url) {
     if (err) {
       return res.send(404);
     }
@@ -37,6 +39,6 @@ app.get('/:alias', function (req, res) {
 
 var server = http.createServer(app);
 
-server.listen(3000, function () {
+server.listen(3000, function() {
   console.log('Node.js server listening on port 3000');
 });
